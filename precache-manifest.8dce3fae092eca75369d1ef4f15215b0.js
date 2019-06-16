7ddb16b18afc3ac159a06c7acdaf6a04509746ce
Ashun Kothari <ashun.kothari10@gmail.com>
2019-06-16 19:26:52 +0530

    Updates

---
+++ precache-manifest.8dce3fae092eca75369d1ef4f15215b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff9ecc534138681a7ea51abc3260d59",
+    "url": "/react-foreign-exchange/index.html"
+  },
+  {
+    "revision": "d4e949d4bbef4c705706",
+    "url": "/react-foreign-exchange/static/css/main.54b94415.chunk.css"
+  },
+  {
+    "revision": "6fb9f888ce55f5a4df79",
+    "url": "/react-foreign-exchange/static/js/2.fba2614a.chunk.js"
+  },
+  {
+    "revision": "d4e949d4bbef4c705706",
+    "url": "/react-foreign-exchange/static/js/main.bbe04c15.chunk.js"
+  },
+  {
+    "revision": "9383bae7603ccbb3989e",
+    "url": "/react-foreign-exchange/static/js/runtime~main.43b31163.js"
+  }
+]);
